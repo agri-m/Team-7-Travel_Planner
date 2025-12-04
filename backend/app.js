af54cc0d53a61e062b9740e4e07c795b1658d017
ajay786jais <ajay.jaiswal_cs23@gla.ac.in>
2025-12-04 12:25:22 +0530

    Added Itenary feature with drag and drop

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const itineraryRoutes = require('./routes/itineraryRoutes');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Database Connection
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/travel-planner')
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('MongoDB connection error:', err));
+
+// Routes
+app.use('/api/v1/itinerary', itineraryRoutes);
 
 app.get('/', (req, res) => {
     res.send('Server is running');
